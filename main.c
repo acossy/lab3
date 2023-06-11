6c808e1bfae9c9dbebd6fe68d7643f228f5995d4
Rene p Dacosta <acoss2@gmail.com>
2023-06-11 22:38:29 +0000

    correction master

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
+int add(int x, int y)
+{
+    return x+y;
+}
+
 int diff(int x, int y)
 {
     return x-y;
 }
+
 int main(int argc, char cons *argv[])
 
 {
+   printf("bonjour");
+   printf("\n ADD=%d", add(3,2));
    printf("\n DIFF=%d", diff(3,2));
   
     return 0;
